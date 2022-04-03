08319182da96d126157118dc3409c90473b5590f
h3rp <88727296+h3rp@users.noreply.github.com>
2022-04-03 11:05:58 +0700

    Update tugas_2-1.cpp

--- tugas_2-1.cpp
+++ tugas_2-1.cpp
@@ -13,7 +13,7 @@ int main(){
     string nama[ukuran];
     int jumlah[ukuran];
     int harga[ukuran];
-    int total = 0;
+    int total[ukuran];
     int total_pembelian = 0;
 
     for (int i = 0; i < ukuran; i++)
@@ -30,19 +30,18 @@ int main(){
     }
      for (int i = 0; i < ukuran; i++)
      {
-        total = jumlah[i] * harga[i];
-        
+        total[i] = jumlah[i] * harga[i];
      }
      for (int i = 0; i < ukuran; i++)
      {
-         total_pembelian = total_pembelian + total;
+         total_pembelian = total_pembelian + total[i];
      }
      
      
     cout << "Kode" << setw(5) << "Nama" << setw(7) << "Jumlah" << setw(7) <<"Harga" << setw(7) <<"Total" << endl;
     for (int i = 0; i < ukuran; i++)
     {
-        cout << kode[i] << setw(7) << nama[i] << setw(7) << jumlah[i] << setw(7) << harga[i] << setw(7) << total << endl;
+        cout << kode[i] << setw(7) << nama[i] << setw(7) << jumlah[i] << setw(7) << harga[i] << setw(13) << total << endl;
     }
     cout << "Jumlah Item adalah : " << ukuran << endl;
     cout << "Total Pembelian adalah : " << total_pembelian << endl;
